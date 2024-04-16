035be84c5922ba6be84f28c4ade96c28f09ae0ca
Ellie22302 <146311968+Ellie22302@users.noreply.github.com>
2024-04-15 22:26:54 -0400

    test

---
+++ index.js
@@ -0,0 +1,47 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const setShape = require('./library/setShape.js')
+const fileName = "logo.svg"
+
+
+const questions = [
+
+    {
+        type: "input",
+        message: "Enter three numbers or letters you would like for the LOGO:",
+        name: "text"
+    },
+    {
+        type: "input",
+        message: "What color would you like the text inside to be: ",
+        name: "textColor",
+      },
+    
+      {
+        type: "list",
+        choices: ["Circle", "Triangle", "Square"],
+        message: "Select the shape that you would like: ",
+        name: "shape",
+      },
+    
+      {
+        type: "input",
+        message: "What color would you like the logo shape to be: ",
+        name: "shapeColor",
+      },
+    ];
+
+    function createLogo(response, fileName) {
+        const svg = setShapea(response);
+        fs.writeFile(fileName, svg, () => console.log("Created " + fileName));
+      }
+      
+      function init() {
+        inquirer
+          .prompt(questions)
+          .then((response) => {
+            createLogo(response, fileName);
+          })
+      }
+      
+      init();
---
+++ library/shapes.test.js
@@ -0,0 +1,37 @@
+const { Circle, Triangle, Square } = require("./shapes.js");
+
+describe("Circle", () => {
+  test(" Circle SVG", () => {
+    const circle = new Circle("test", "test", "test");
+    expect(circle.render()).toEqual(
+      `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="150" cy="100" r="80" fill="test"/>
+              <text x="150" y="115" font-size="60" text-anchor="middle" fill="test">test</text>
+            </svg>`
+    );
+  });
+});
+
+describe("Triangle", () => {
+  test("Triangle SVG", () => {
+    const triangle = new Triangle("test", "test", "test");
+    expect(triangle.render()).toEqual(
+      `<svg version="1.1" width="300" height="200">
+              <polygon points="150,45 300,300 0,300" fill="test"/>
+              <text x="150" y="155" font-size="60" text-anchor="middle" fill="test">test</text>
+            </svg>`
+    );
+  });
+});
+
+describe("Square", () => {
+  test(" Square SVG", () => {
+    const square = new Square("test", "test", "test");
+    expect(square.render()).toEqual(
+      `<svg version="1.1" width="300" height="200">
+              <rect width="100%" height="100%" fill="test"/>
+              <text x="150" y="120" font-size="60" text-anchor="middle" fill="test">test</text>
+            </svg>`
+    );
+  });
+});
